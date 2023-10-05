8e06167cf5a1c218a5f24bf5038dfbdd2a7de37a
Jaderm1 <jadermaciel1@gmail.com>
2023-10-05 20:57:32 -0300

    Exercício 06 concluído

---
+++ src/main/java/exercicio07/Main.java
@@ -0,0 +1,41 @@
+package exercicio07;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Insira o primeiro valor");
+        double valor1 = input.nextDouble();
+        System.out.println("Insira o segundo valor");
+        double valor2 = input.nextDouble();
+        System.out.println("Insira a opração");
+        String operacao = input.next();
+        double resultado = 0;
+        switch (operacao) {
+            case "+":
+                resultado = valor1 + valor2;
+                break;
+            case "-":
+                resultado = valor1 - valor2;
+                break;
+            case "*":
+                resultado = valor1 * valor2;
+                break;
+            case "/":
+                if(valor2 == 0){
+                    System.out.println("Não é possível dividir por 0");
+                    break;
+                }
+                resultado = valor1 / valor2;
+                break;
+            default:
+                System.out.println("Operação inválida");
+                break;
+        }
+        System.out.println("O resultado é: " + resultado);
+
+    }
+
+
+}
