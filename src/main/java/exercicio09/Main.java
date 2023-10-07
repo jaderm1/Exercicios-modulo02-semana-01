9391c713647ced8d27cb25d2d496f2240c48236a
Jaderm1 <jadermaciel1@gmail.com>
2023-10-06 23:17:26 -0300

    Exercício 09 concluído

---
+++ src/main/java/exercicio09/Main.java
@@ -0,0 +1,26 @@
+package exercicio09;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Insira o salário do funcionário");
+        Scanner salarioInput = new Scanner(System.in);
+        double salario = salarioInput.nextDouble();
+        double novoSalario = 0;
+
+        if (salario <= 1200) {
+            novoSalario = salario + (salario * 0.2);
+            System.out.printf("O salário antes do reajuste era de R$%.2f, o percentual de aumento aplicado foi de 20%%, o valor do aumento foi de R$%.2f e o novo salário é de R$%.2f", salario, (salario * 0.2), novoSalario);
+        } else if (salario > 1200 && salario <= 1700) {
+            novoSalario = salario + (salario * 0.15);
+            System.out.printf("O salário antes do reajuste era de R$%.2f, o percentual de aumento aplicado foi de 15%%, o valor do aumento foi de R$%.2f e o novo salário é de R$%.2f", salario, (salario * 0.15), novoSalario);
+        } else if (salario > 1700 && salario <= 2500) {
+            novoSalario = salario + (salario * 0.1);
+            System.out.printf("O salário antes do reajuste era de R$%.2f, o percentual de aumento aplicado foi de 10%%, o valor do aumento foi de R$%.2f e o novo salário é de R$%.2f", salario, (salario * 0.1), novoSalario);
+        } else if (salario > 2500) {
+            novoSalario = salario + (salario * 0.05);
+            System.out.printf("O salário antes do reajuste era de R$%.2f, o percentual de aumento aplicado foi de 5%%, o valor do aumento foi de R$%.2f e o novo salário é de R$%.2f", salario, (salario * 0.05), novoSalario);
+        }
+
+    }
+}
